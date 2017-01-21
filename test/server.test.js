44315f6b73d17b1c9211e775c3f2a16b4026462c
Jayder França <jayderfranca@gmail.com>
2017-01-21 03:13:25 -0200

    Desenvolvimento da estrutura inicial

---
+++ test/server.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mocha = require('mocha');
+var chai = require('chai');
+
+chai.should();
+
+mocha.describe('Empty', function () {
+  mocha.it('=> Empty', function () {
+  })
+});
